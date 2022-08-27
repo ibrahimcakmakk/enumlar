2e324a16ab92ac04beb28137df875e8feb39bf35
ibrahimcakmakk <ibrahim.cakmak@stu.fsm.edu.tr>
2022-08-27 12:31:54 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+
+class Program{
+
+    static void Main(string[] args){
+
+        Console.WriteLine(Günler.pazar);
+        Console.WriteLine((int)Günler.cumartesi);
+
+        int sıcaklık = 32;
+
+        if (sıcaklık <= HavaDurumu.Normal)
+        {
+        Console.WriteLine("Dışarı çıkmak için çok sıcak");
+            
+        }else if(sıcaklık >= (int)HavaDurumu.Sıcak){
+            
+            Console.WriteLine("dışarı çıkmak için çok sıcak bir gün");
+
+        }else if(sıcaklık>=(int)HavaDurumu.Normal && sıcaklık<(int)HavaDurumu.Coksıcak){
+            Console.WriteLine("Hadi Dışarı Çıkalım");
+        }
+
+    }
+}
+
+enum Günler{
+
+    pazartesi = 1,
+    salı,
+    carşamba,
+    perşembe,
+    cuma = 23,
+    cumartesi,
+    pazar,
+}
+
+enum HavaDurumu{
+    Soguk = 5,
+    Normal = 20,
+    Sıcak = 25,
+    Coksıcak = 30,
+}
